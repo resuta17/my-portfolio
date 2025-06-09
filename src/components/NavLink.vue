74c6d3a81b66b45e5ecfae57a3f67fd499fa784a
Lester <jlhebres15@gmail.com>
2025-06-10 01:03:36 +0800

    first commit

---
+++ src/components/NavLink.vue
@@ -0,0 +1,27 @@
+<template>
+  <a
+    :href="href"
+    :class="[
+      active ? 'text-teal-300 font-semibold' : 'text-white hover:text-teal-200',
+      'px-3 py-2 rounded-md text-sm font-medium transition-colors duration-200'
+    ]"
+    @click="$emit('click')"
+  >
+    <slot />
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    href: {
+      type: String,
+      required: true
+    },
+    active: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
